263347b18152689611902d4f423c292d2143ef2a
Bemo988 <54604456+Bemo988@users.noreply.github.com>
2019-08-28 00:30:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`nothing`,"http://twitch.tv/omargamer89")
 client.user.setStatus("dnd")
 });
 
